39ed413092312866f54081f7f53e2114411264ee
Djelil14 <ichaoudjelil@gmail.com>
2025-11-04 12:35:23 +0100

    ajout model du commentaire

---
+++ app/Models/Comment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'article_id',
+        'author_name',
+        'author_email',
+        'content',
+        'approved',
+    ];
+
+    protected $casts = [
+        'approved' => 'boolean',
+    ];
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
+
+
