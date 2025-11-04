f30d43349458a10f8ca85b65652d4cd14cf6de11
Djelil14 <ichaoudjelil@gmail.com>
2025-11-04 12:32:22 +0100

    ajout des migrations

---
+++ database/migrations/2025_09_25_000000_add_metrics_to_articles_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->unsignedBigInteger('impressions')->default(0)->after('date_publication');
+            $table->unsignedBigInteger('views')->default(0)->after('impressions');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn(['impressions', 'views']);
+        });
+    }
+};
+
+
